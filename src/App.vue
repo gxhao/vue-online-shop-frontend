223a40b51b6969996c27efdd4f3b190fbae25b6d
gxhao <1625546088@qq.com>
2022-02-09 16:35:49 +0800

    001-vue-frontend build

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <nav>
+      <div class="container">
+        <ul class="nav__left">
+          <li>
+            <router-link to="/">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/admin">Admin</router-link>
+          </li>
+          <li>
+            <router-link to="/cart">Cart</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: 'App',
+};
 </script>
 
 <style>
