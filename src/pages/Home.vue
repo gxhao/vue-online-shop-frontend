ab966a03bc44b03f066764b5b204af4b8e8a2252
gxhao <1625546088@qq.com>
2022-02-17 14:12:37 +0800

    1、vuex version modified 3.6.2 2、 vuex configration usage

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div>
     <div class="title">
-      <h1>{{msg}}</h1>
+      <h1>In Stock</h1>
     </div>
+    <product-list></product-list>
   </div>
 </template>
 
 <script>
+import ProductList from '@/components/ProductList.vue';
   export default {
-    name: 'Home',
+    name: 'home',
     data () {
       return {
         msg: 'Welcome to Your Vue.js App'
       };
     },
+    components: {
+      'product-list': ProductList
+    }
   }
 </script>
